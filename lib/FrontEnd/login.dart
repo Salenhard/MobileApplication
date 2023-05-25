8e60e805196b62e31423dabbe0759c8e54ccc175
NemoNology <chicken.gang@mail.ru>
2023-05-26 04:04:17 +0900

    Restarted creating UI - Build 0.75

--- lib/FrontEnd/login.dart
+++ lib/FrontEnd/login.dart
@@ -1,34 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:untitled4/BackEnd/database/database.dart';
-import '../BackEnd/database/client_model.dart';
 import '/Other/extensions.dart';
 
 class Login extends StatefulWidget {
-  final List<Client> list;
-  const Login(this.list, {super.key});
+  const Login({super.key});
 
   @override
-  State<StatefulWidget> createState() => LoginState(list);
+  State<StatefulWidget> createState() => LoginState();
 }
 
 class LoginState extends State<Login> {
-  final List<Client> list;
   final _formKey = GlobalKey<FormState>();
   String mail = "";
   String password = "";
 
-  LoginState(this.list);
+  LoginState();
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Center(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        key: _formKey,
+        child: Center(
+
+            // TODO: Center buttons alignment
+
+            child: Wrap(
+              alignment: WrapAlignment.start,
+              crossAxisAlignment: WrapCrossAlignment.start,
+                children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 100, horizontal: 10),
+                  // color: Colors.amber,
+                  child: const Text("Hello there.",
+                      style: TextStyle(
+                          color: Extensions.colorBright,
+                          fontWeight: FontWeight.bold),
+                      textScaleFactor: 5),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: MediaQuery.of(context).size.width * 0.15),
+                child: TextField(
+                  key: const Key("inputMail"),
+                  style: const TextStyle(color: Extensions.colorSmooth2),
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: Extensions.getTextFormFieldDecoration1("email"),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: MediaQuery.of(context).size.width * 0.15),
+                child: TextField(
+                  autocorrect: false,
+                  obscureText: true,
+                  style: const TextStyle(color: Extensions.colorSmooth2),
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration:
+                      Extensions.getTextFormFieldDecoration1("password"),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/profile');
+                        },
+                        style: Extensions.buttonStyleUsual1,
+                        child: const Text('Log In',
+                            style: TextStyle(color: Extensions.colorBright))),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/profile');
+                        },
+                        style: Extensions.buttonStyleUsual1,
+                        child: const Text('Sign Up',
+                            style: TextStyle(color: Extensions.colorBright))),
+                  ],
+                ),
+              ),
+
+              /*
+
+        Column(
         children: [
-          Container(
-              padding: const EdgeInsets.all(50.0),
-              child: const FittedBox(
+          const Expanded(
+              child: FittedBox(
                 child: Text("Sign In",
                     style: TextStyle(
                         color: Extensions.colorBright,
@@ -72,11 +135,10 @@ class LoginState extends State<Login> {
               ElevatedButton(
                   onPressed: () {
                     for (int i = 0; i < list.length; i++) {
-                      if (mail == list[i].mail && password == list[i].password) {
-                          Navigator.pushNamed(context, '/profile');
-                      }
-                      else {
-
+                      if (mail == list[i].mail &&
+                          password == list[i].password) {
+                        Navigator.pushNamed(context, '/profile');
+                      } else {
                         // wrong
                       }
                     }
@@ -96,7 +158,9 @@ class LoginState extends State<Login> {
             ]),
           )
         ],
-      )),
-    );
+      )
+
+        */
+            ])));
   }
 }
