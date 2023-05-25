38b9c5faab53475e7f48cbb239561fb3483ecd33
NemoNology <chicken.gang@mail.ru>
2023-05-25 19:44:18 +0900

    Improved data base path finding

--- lib/BackEnd/database/database.dart
+++ lib/BackEnd/database/database.dart
@@ -1,6 +1,4 @@
-import 'dart:io';
 import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'client_model.dart';
 
@@ -11,38 +9,44 @@ class DatabaseHelper {
   Future<Database> get database async => _database ??= await _initdatabase();
 
   Future<Database> _initdatabase() async {
-    Directory documentsDiretory = await getApplicationDocumentsDirectory();
+    // Directory documentsDiretory = await getApplicationDocumentsDirectory();
+    var dataBasesPath = await getDatabasesPath();
 
-    String path = join(documentsDiretory.path, "Clients.db");
+    // String path = join(documentsDiretory.path, "Clients.db");
+    String path = join(dataBasesPath, "Clients.db");
     return await openDatabase(
       path,
       version: 1,
       onCreate: _onCreate,
-      );
-  }
-  Future _onCreate(Database db, int version) async {
-    await db.execute(
-        "CREATE TABLE Clients("
-            "id INTEGER PRIMARY KEY, "
-            "name TEXT, "
-            "mail TEXT, "
-            "password TEXT, "
-            "age INTEGER"
-            ")"
     );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute("CREATE TABLE Clients("
+        "id INTEGER PRIMARY KEY, "
+        "name TEXT, "
+        "mail TEXT, "
+        "password TEXT, "
+        "age INTEGER"
+        ")");
   }
 
   Future<List<Client>> getClients() async {
     Database db = await instance.database;
     var clients = await db.query("Clients", orderBy: "id");
-    List<Client> clientlist = clients.isNotEmpty ? clients.map((c) => Client.fromJson(c)).toList() : [];
+    List<Client> clientlist = clients.isNotEmpty
+        ? clients.map((c) => Client.fromJson(c)).toList()
+        : [];
     return clientlist;
   }
 
   Future<Client> searchByMail(String mail) async {
     Database db = await instance.database;
-    var clients = await db.query("Clients", where: "mail = ?", whereArgs: [mail]);
-    List<Client> clientlist = clients.isNotEmpty ? clients.map((c) => Client.fromJson(c)).toList() : [];
+    var clients =
+        await db.query("Clients", where: "mail = ?", whereArgs: [mail]);
+    List<Client> clientlist = clients.isNotEmpty
+        ? clients.map((c) => Client.fromJson(c)).toList()
+        : [];
     Client client = clientlist[0];
     return client;
   }
@@ -50,7 +54,9 @@ class DatabaseHelper {
   Future<int> add(Client client) async {
     Database db = await instance.database;
     var clients = await db.query("Clients", orderBy: "id");
-    List<Client> clientlist = clients.isNotEmpty ? clients.map((c) => Client.fromJson(c)).toList() : [];
+    List<Client> clientlist = clients.isNotEmpty
+        ? clients.map((c) => Client.fromJson(c)).toList()
+        : [];
     int id = clientlist.length + 1;
     client.id = id;
     return await db.insert("Clients", client.toJson());
@@ -63,7 +69,7 @@ class DatabaseHelper {
 
   Future<int> update(Client client) async {
     Database db = await instance.database;
-    return await db.update("Clients", client.toJson(), where: 'id = ?', whereArgs: [client.id]);
+    return await db.update("Clients", client.toJson(),
+        where: 'id = ?', whereArgs: [client.id]);
   }
 }
-
