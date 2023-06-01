fc5e34f9503b75f8a0b48e5bad1c519f6c6d7fc0
NemoNology <chicken.gang@mail.ru>
2023-06-01 11:50:26 +0900

    Build 1.0

--- lib/FrontEnd/login.dart
+++ lib/FrontEnd/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:untitled4/BackEnd/database/database.dart';
+import 'package:untitled4/BackEnd/database/data_base.dart';
+import 'package:untitled4/FrontEnd/main_menu.dart';
 import '/Other/extensions.dart';
 
 class Login extends StatefulWidget {
@@ -10,160 +11,159 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
-
   final _mailTEC = TextEditingController();
   final _passwordTEC = TextEditingController();
 
   var _isSomeFieldIsEmpty = false;
   var _isInputsIsWrong = false;
 
+  static int id = 0;
+
   LoginState();
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: _formKey,
         child: SingleChildScrollView(
-          child: Wrap(crossAxisAlignment: WrapCrossAlignment.start, children: [
-            // Title
+      child: Wrap(crossAxisAlignment: WrapCrossAlignment.start, children: [
+        // Title
 
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 100, horizontal: 10),
-                // color: Colors.amber,
-                child: const Text("Hello there.",
-                    style: Extensions.textStyleMainField1, textScaleFactor: 5),
-              ),
-            ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 10),
+            // color: Colors.amber,
+            child: const Text("Hello there.",
+                style: Extensions.textStyleMainField1, textScaleFactor: 5),
+          ),
+        ),
 
-            // Inputs
+        // Inputs
 
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: MediaQuery.of(context).size.width * 0.15),
-                  child: TextField(
-                    controller: _mailTEC,
-                    style: const TextStyle(color: Extensions.colorSmooth2),
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: Extensions.getTextFormFieldDecoration1("email"),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: MediaQuery.of(context).size.width * 0.15),
-                  child: TextField(
-                    controller: _passwordTEC,
-                    autocorrect: false,
-                    obscureText: true,
-                    style: const TextStyle(color: Extensions.colorSmooth2),
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration:
-                        Extensions.getTextFormFieldDecoration1("password"),
-                  ),
-                )
-              ],
-            ),
-
-            // Warnings
-
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Visibility(
-                    visible: _isSomeFieldIsEmpty,
-                    child: const Text(
-                      "Input email and password",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Visibility(
-                    visible: _isInputsIsWrong,
-                    child: const Text(
-                      "Wrong email and\\or password",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                )
-              ],
-            ),
-
-            // Buttons
-
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
             Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    child: ElevatedButton(
-                        onPressed: () async {
-                          var mail = _mailTEC.text;
-                          var password = _passwordTEC.text;
-
-                          if (mail.isEmpty || password.isEmpty) {
-                            setState(() {
-                              _isSomeFieldIsEmpty = true;
-                            });
-                            return;
-                          }
-                          setState(() {
-                            _isSomeFieldIsEmpty = false;
-                          });
-
-                          try {
-                            if (!(await ClientsDataBase
-                                .isThereClientByLoginInfo(mail, password))) {
-                              setState(() {
-                                _isInputsIsWrong = true;
-                              });
-                              return;
-                            }
-                          } catch (e) {
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text(
-                                    "Something was wrong with data base\nError: $e")));
-                          }
-
-                          setState(() {
-                            _isInputsIsWrong = false;
-                          });
-
-                          // todo: runApp(MainMenu());
-                        },
-                        style: Extensions.buttonElevatedStyleUsual1,
-                        child: const Text('Log In',
-                            style: Extensions.textStyleUsual1)),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/registration');
-                        },
-                        style: Extensions.buttonElevatedStyleUsual2,
-                        child: const Text('Sign Up',
-                            style: Extensions.textStyleUsual1)),
-                  )
-                ],
+              padding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: MediaQuery.of(context).size.width * 0.15),
+              child: TextField(
+                controller: _mailTEC,
+                style: const TextStyle(color: Extensions.colorSmooth2),
+                textAlignVertical: TextAlignVertical.center,
+                decoration: Extensions.getTextFormFieldDecoration1("email"),
               ),
             ),
-          ]),
-        ));
+            Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: MediaQuery.of(context).size.width * 0.15),
+              child: TextField(
+                controller: _passwordTEC,
+                autocorrect: false,
+                obscureText: true,
+                style: const TextStyle(color: Extensions.colorSmooth2),
+                textAlignVertical: TextAlignVertical.center,
+                decoration: Extensions.getTextFormFieldDecoration1("password"),
+              ),
+            )
+          ],
+        ),
+
+        // Warnings
+
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Visibility(
+                visible: _isSomeFieldIsEmpty,
+                child: const Text(
+                  "Input email and password",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Visibility(
+                visible: _isInputsIsWrong,
+                child: const Text(
+                  "Wrong email and\\or password",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            )
+          ],
+        ),
+
+        // Buttons
+
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: ElevatedButton(
+                    onPressed: () async {
+                      var mail = _mailTEC.text.replaceAll(r' ', '');
+                      var password = _passwordTEC.text.replaceAll(r' ', '');
+
+                      if (mail.isEmpty || password.isEmpty) {
+                        setState(() {
+                          _isSomeFieldIsEmpty = true;
+                        });
+                        return;
+                      }
+                      setState(() {
+                        _isSomeFieldIsEmpty = false;
+                      });
+
+                      try {
+                        if (!(await ClientsDataBase.isThereClientByLoginInfo(
+                            mail, password))) {
+                          setState(() {
+                            _isInputsIsWrong = true;
+                          });
+                          return;
+                        } else {
+                          var user = await ClientsDataBase.findClientByID(id);
+
+                          runApp(MainMenu(user: user));
+                        }
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            Extensions.getSnackBar(
+                                "Something was wrong with data base\nError: $e"));
+                      }
+
+                      setState(() {
+                        _isInputsIsWrong = false;
+                      });
+                    },
+                    style: Extensions.buttonElevatedStyleUsual1,
+                    child: const Text('Log In',
+                        style: Extensions.textStyleUsual1)),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/registration');
+                    },
+                    style: Extensions.buttonElevatedStyleUsual2,
+                    child: const Text('Sign Up',
+                        style: Extensions.textStyleUsual1)),
+              )
+            ],
+          ),
+        ),
+      ]),
+    ));
   }
 }
