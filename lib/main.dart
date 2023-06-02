da01d4821bdafdeb65a4fb7419837a42e15e2031
NemoNology <chicken.gang@mail.ru>
2023-06-02 14:58:50 +0900

    Replaced login menu widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:untitled4/BackEnd/database/data_base.dart';
-import '/FrontEnd/registration.dart';
-import '/FrontEnd/login.dart';
-import '/Other/extensions.dart';
+import 'FrontEnd/login_menu.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   ClientsDataBase.init();
   
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/',
-    routes: {
-      '/': (BuildContext context) =>
-          const Scaffold(backgroundColor: Extensions.colorDark, body: Login()),
-      '/registration': (BuildContext context) => const Scaffold(
-          backgroundColor: Extensions.colorDark, body: Registration()),
-    },
-  ));
+  runApp(const LoginMenu());
 }
