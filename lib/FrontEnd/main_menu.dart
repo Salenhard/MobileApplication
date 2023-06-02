319f6e1796ce26abc8b915d6318e03e20c37d55e
NemoNology <chicken.gang@mail.ru>
2023-06-02 23:07:27 +0900

    Build 0.7

--- lib/FrontEnd/main_menu.dart
+++ lib/FrontEnd/main_menu.dart
@@ -1,45 +1,64 @@
 import 'package:flutter/material.dart';
-import 'package:untitled4/BackEnd/database/data_base.dart';
-import '../BackEnd/database/client_model.dart';
+import 'package:untitled4/FrontEnd/calculator.dart';
+import '../BackEnd/database/data_base.dart';
 import '../Other/extensions.dart';
 
-class MainMenu extends StatelessWidget {
-  final Client user;
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
 
-  const MainMenu({required this.user, super.key});
+  @override
+  State<StatefulWidget> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/',
-        routes: {
-          '/': (BuildContext context) => Scaffold(
-              backgroundColor: Extensions.colorDark,
-              body: MainForm(user: user)),
-        });
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          backgroundColor: Extensions.colorSmooth1,
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (newIndex) {
+            setState(() {
+              currentPageIndex = newIndex;
+            });
+          },
+          destinations: const [
+            NavigationDestination(
+                icon: Icon(Icons.home, color: Extensions.colorSmooth2),
+                label: "Home"),
+            NavigationDestination(
+                icon: Icon(Icons.calculate, color: Extensions.colorSmooth2),
+                label: "Calculator")
+          ],
+        ),
+        backgroundColor: Extensions.colorDark,
+        body: const [MainForm(), Calculator()][currentPageIndex],
+      ),
+    );
   }
 }
 
 class MainForm extends StatelessWidget {
-  final Client user;
-
-  const MainForm({required this.user, super.key});
+  const MainForm({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Form(
         child: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
+      child: Wrap(
+        crossAxisAlignment: WrapCrossAlignment.start,
         children: [
+          // Title
+
           Container(
             margin: const EdgeInsets.symmetric(vertical: 60, horizontal: 10),
             alignment: Alignment.centerLeft,
-            child: Text("Hi, ${user.name}.",
-                style: Extensions.textStyleMainField1, textScaleFactor: 2),
+            child: Text("Hi, ${ClientsDataBase.user!.name}.",
+                style: Extensions.textStyleMainField1, textScaleFactor: 4),
           ),
         ],
       ),
