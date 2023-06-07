96fc7191f72021c0c3bd2b6db9b0e0a5507a4336
NemoNology <chicken.gang@mail.ru>
2023-06-08 00:59:17 +0900

    Started calculator: added output views

--- lib/FrontEnd/calculator.dart
+++ lib/FrontEnd/calculator.dart
@@ -1,46 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:untitled4/Other/extensions.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final _expresion = "0";
+  final _result = "0";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: key,
-      backgroundColor: Extensions.colorDark,
-      body: Container(
-          margin: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.35,
-              vertical: MediaQuery.of(context).size.width * 0.65),
-          child: ElevatedButton(
-            onPressed: () async {
-              var uri = Uri(path: 'calculator://');
-              if (await canLaunchUrl(uri)) {
-                try {
-                  await launchUrl(uri);
-                } catch (e) {
-                  // ignore: use_build_context_synchronously
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      Extensions.getSnackBar(
-                          "Can't launch canculator\nError: $e)"));
-                }
-              } else {
-                // ignore: use_build_context_synchronously
-                ScaffoldMessenger.of(context).showSnackBar(
-                    Extensions.getSnackBar(
-                        "Can't launch canculator (Used URL: $uri)"));
-              }
-            },
-            child: const FittedBox(
-              alignment: Alignment.center,
-              child: Text(
-                "Launch system calculator",
-                style: Extensions.textStyleMainField1,
+        backgroundColor: Extensions.colorDark,
+        appBar: AppBar(
+          backgroundColor: Extensions.colorSmooth1,
+          shadowColor: Colors.transparent,
+          title: const Text(
+            "Simple caclulator.",
+            style: Extensions.textStyleMainField1,
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                        color: Extensions.colorSmooth2,
+                        padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.05),
+                        child: FittedBox(
+                          child: Text(
+                            _expresion,
+                            textAlign: TextAlign.right,
+                            style: Extensions.textStyleMainField1,
+                          ),
+                        )),
+                  ),
+                ],
               ),
-            ),
-          )),
-    );
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                        color: Colors.transparent,
+                        padding: EdgeInsets.symmetric(
+                            horizontal:
+                                MediaQuery.of(context).size.width * 0.05),
+                        child: FittedBox(
+                          child: Text(
+                            _result,
+                            textAlign: TextAlign.right,
+                            style: Extensions.textStyleMainField1,
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+        // Wrap(
+        //   direction: Axis.vertical,
+        //   alignment: WrapAlignment.start,
+        //   children: [
+
+        //   ],
+        // ),
+        );
   }
 }
