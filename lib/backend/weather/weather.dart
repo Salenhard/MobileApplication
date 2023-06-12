756f13a3f3a838fed4254d8d2633fe143fb38dfd
NemoNology <chicken.gang@mail.ru>
2023-06-12 13:30:01 +0900

    Private -> public

--- lib/backend/weather/weather.dart
+++ lib/backend/weather/weather.dart
@@ -2,14 +2,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Weather {
-  static const _apiKey = "7cf3fe86c6e54d14a8e234419231106";
-  static const _cityName = "Chita";
+  static const apiKey = "7cf3fe86c6e54d14a8e234419231106";
+  static const cityName = "Chita";
 
   static Future<String> weather({required Function(Object) onError}) async {
     String res = "";
 
     const url =
-        "https://api.weatherapi.com/v1/current.json?key=$_apiKey&q=$_cityName&aqi=no";
+        "https://api.weatherapi.com/v1/current.json?key=$apiKey&q=$cityName&aqi=no";
 
     try {
       var body = json.decode((await http.get(Uri.parse(url))).body)["current"];
