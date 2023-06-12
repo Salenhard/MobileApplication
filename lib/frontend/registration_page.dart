93b6d63a50b23841b887989b80ab1ce3a7043c64
NemoNology <chicken.gang@mail.ru>
2023-06-12 10:35:16 +0900

    Improved page - B 1.5

--- lib/frontend/registration_page.dart
+++ lib/frontend/registration_page.dart
@@ -20,114 +20,116 @@ class RegistrationState extends State<Registration> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          crossAxisAlignment: WrapCrossAlignment.start,
-          children: [
-            // Button back
+    return Scaffold(
+      backgroundColor: Extensions.colorDark,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        iconTheme: const IconThemeData(color: Extensions.colorBright),
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Wrap(
+            alignment: WrapAlignment.center,
+            crossAxisAlignment: WrapCrossAlignment.start,
+            children: [
+              // Title
 
-            AppBar(
-              backgroundColor: Colors.transparent,
-              shadowColor: Colors.transparent,
-              iconTheme: const IconThemeData(color: Extensions.colorBright),
-            ),
-
-            // Title
-
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(10, 40, 10, 60),
-                // color: Colors.amber,
-                child: const Text("Registration.",
-                    style: Extensions.textStyleMainField1, textScaleFactor: 3),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+                child: const FittedBox(
+                  child: Text("Registration.",
+                      style: Extensions.textStyleMainField1,
+                      textScaleFactor: 3),
+                ),
               ),
-            ),
 
-            // Inputs
+              // Inputs
 
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Extensions.getFullInputFieldAsColumn(
-                    "Name",
-                    Extensions.getTextFormFieldValidator("name", RegExp(r'.+')),
-                    _nameTEC),
-                Extensions.getFullInputFieldAsColumn(
-                    "Email",
-                    Extensions.getTextFormFieldValidator(
-                        "email", RegExp(r'(.+)+(@)+(.{1})+(\.)+.+')),
-                    _mailTEC),
-                Extensions.getFullInputFieldAsColumn(
-                    "Password",
-                    Extensions.getTextFormFieldValidator(
-                        "password",
-                        RegExp(
-                            r'^(?=.*[^ ])(?=.*?[A-ZА-Я])(?=.*?[a-zа-я])(?=.*?[0-9]).{8,}$')),
-                    _password1TEC,
-                    isPassword: true),
-                Extensions.getFullInputFieldAsColumn("Repeat password",
-                    (value) {
-                  if (value!.isEmpty) {
-                    return "Repeat password";
-                  }
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Extensions.getFullInputFieldAsColumn(
+                      "Name",
+                      Extensions.getTextFormFieldValidator(
+                          "name", RegExp(r'.+')),
+                      _nameTEC),
+                  Extensions.getFullInputFieldAsColumn(
+                      "Email",
+                      Extensions.getTextFormFieldValidator(
+                          "email", RegExp(r'(.+)+(@)+(.{1})+(\.)+.+')),
+                      _mailTEC),
+                  Extensions.getFullInputFieldAsColumn(
+                      "Password",
+                      Extensions.getTextFormFieldValidator(
+                          "password",
+                          RegExp(
+                              r'^(?=.*[^ ])(?=.*?[A-ZА-Я])(?=.*?[a-zа-я])(?=.*?[0-9]).{8,}$')),
+                      _password1TEC,
+                      isPassword: true),
+                  Extensions.getFullInputFieldAsColumn("Repeat password",
+                      (value) {
+                    if (value!.isEmpty) {
+                      return "Repeat password";
+                    }
 
-                  if (value != _password1TEC.text) {
-                    return "Password does not match";
-                  }
+                    if (value != _password1TEC.text) {
+                      return "Password does not match";
+                    }
 
-                  return null;
-                }, _password2TEC, isPassword: true),
-              ],
-            ),
+                    return null;
+                  }, _password2TEC, isPassword: true),
+                ],
+              ),
 
-            // Button
+              // Button
 
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      var mail = _mailTEC.text.replaceAll(r' ', '');
-                      var password = _password1TEC.text.replaceAll(r' ', '');
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        var mail = _mailTEC.text.replaceAll(r' ', '');
+                        var password = _password1TEC.text.replaceAll(r' ', '');
 
-                      try {
-                        if (await ClientsDataBase.isThereClientByMail(mail)) {
+                        try {
+                          if (await ClientsDataBase.isThereClientByMail(mail)) {
+                            // ignore: use_build_context_synchronously
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                Extensions.getSnackBar(
+                                    "This email is already taken. Please enter a different email"));
+                            return;
+                          }
+
+                          await ClientsDataBase.insert(Client(
+                              name: _nameTEC.text,
+                              mail: mail,
+                              password: password));
+
                           // ignore: use_build_context_synchronously
                           ScaffoldMessenger.of(context).showSnackBar(
                               Extensions.getSnackBar(
-                                  "This email is already taken. Please enter a different email"));
-                          return;
+                                  "Registration successful, login to continue"));
+
+                          // ignore: use_build_context_synchronously
+                          Navigator.pop(context);
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              Extensions.getSnackBar(
+                                  "Something was wrong with data base\nError: $e"));
                         }
-
-                        await ClientsDataBase.insert(Client(
-                            name: _nameTEC.text,
-                            mail: mail,
-                            password: password));
-
-                        // ignore: use_build_context_synchronously
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            Extensions.getSnackBar(
-                                "Registration successful, login to continue"));
-
-                        // ignore: use_build_context_synchronously
-                        Navigator.pop(context);
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            Extensions.getSnackBar(
-                                "Something was wrong with data base\nError: $e"));
                       }
-                    }
-                  },
-                  style: Extensions.buttonElevatedStyleUsual1,
-                  child:
-                      const Text('Sign Up', style: Extensions.textStyleUsual1)),
-            ),
-          ],
+                    },
+                    style: Extensions.buttonElevatedStyleUsual1,
+                    child: const Text('Sign Up',
+                        style: Extensions.textStyleUsual1)),
+              ),
+            ],
+          ),
         ),
       ),
     );
